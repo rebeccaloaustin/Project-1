d9fea9a95288955259782dbaf4d1d1e8bdba8f96
rebeccaloaustin <rebeccaloaustin@gmail.com>
2023-10-07 17:01:20 -0400

    button displays and javascript outline created

---
+++ simon.js
@@ -0,0 +1,16 @@
+//define variables:
+//array for color options
+//array for simon sequence
+//array for user sequence
+//current level
+
+
+//event listener to start game
+
+//function to generate a pattern by simon
+//function to highlight button and display simon's pattern
+//function to allow user input to be stored in the empty array for user sequence
+//function to check user input
+//function to keep track of the current level
+//function to increase the speed of the button highlight as the levels increase
+//function to check for end of game (when level 15 is reached)
